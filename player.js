d0bdc7890be9f7e371e032447c3d9a6619caf934
jecklzh <40534273+jecklzh@users.noreply.github.com>
2025-07-23 00:32:58 +0800

    Update player.js

--- player.js
+++ player.js
@@ -197,7 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fadeOut(callback) {
       const initialVolume = this.dom.audio.volume;
       //if (initialVolume === 0) { if (this.state.isPausing) this.dom.audio.pause(); if (callback) callback(); return; }
-      if (initialVolume === 0) { setTimeout(() => { if (this.state.isPausing) this.dom.audio.pause(); if (callback) callback(); }, 300); return; }
+      if (initialVolume === 0) { setTimeout(() => { if (this.state.isPausing) this.dom.audio.pause(); if (callback) callback(); }, 450); return; }
       clearInterval(this.state.fadeInterval); const step = initialVolume / 10, interval = 25, audio = this.dom.audio;
       this.state.fadeInterval = setInterval(() => {
         if (audio.volume > step) { audio.volume = Math.max(0, audio.volume - step); } 
